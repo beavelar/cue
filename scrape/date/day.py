aec3e72fa11f6b3ae929240be4991f099d74fd56
Brian <beavelar.github@gmail.com>
2021-09-12 15:13:44 -0600

    Addition of logs for scraper

--- scrape/date/day.py
+++ scrape/date/day.py
@@ -26,16 +26,19 @@ class day:
 	SATURDAY=5
 	SUNDAY=6
 
-	def __init__(self, day_input):
-		if day_input == self.MONDAY:
-			self.day = 'Monday'
-		elif day_input == self.TUESDAY:
-			self.day = 'Tuesday'
-		elif day_input == self.WEDNESDAY:
-			self.day = 'Wednesday'
-		elif day_input == self.THURSDAY:
-			self.day = 'Thursday'
-		elif day_input == self.FRIDAY:
-			self.day = 'Friday'
+	def __init__(self, day_input: int) -> None:
+		if isinstance(day_input, int):
+			if day_input == self.MONDAY:
+				self.day = 'Monday'
+			elif day_input == self.TUESDAY:
+				self.day = 'Tuesday'
+			elif day_input == self.WEDNESDAY:
+				self.day = 'Wednesday'
+			elif day_input == self.THURSDAY:
+				self.day = 'Thursday'
+			elif day_input == self.FRIDAY:
+				self.day = 'Friday'
+			else:
+				self.day = 'INVALID'
 		else:
-			self.day = 'INVALID'
+			raise TypeError('Non-int parameter passed for day_input when creating day, unable to create day')
