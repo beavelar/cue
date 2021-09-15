5c1a60afa4e5956e5cf71a5ecb62af16573d48e9
Brian <beavelar.github@gmail.com>
2021-09-14 20:17:04 -0600

    Addition of docstring for environment

--- scrape/environment/environment.py
+++ scrape/environment/environment.py
@@ -10,7 +10,27 @@ logger = logging.getLogger(__name__)
 #########################################################################################################
 
 class environment:
+	'''
+	environment
+	----------
+
+	This class will contain the environment variables for the scraper:
+
+	- input_file
+
+	- output_file
+
+	- data_directory
+	'''
 	def __init__(self):
+		'''
+		__init__
+		----------
+
+		Creates a environment object. If an exception is raised retrieving environment variables, all
+
+		elements of the environment class will be set to an empty string
+		'''
 		logger.info('Retrieving environment variables')
 		try:
 			load_dotenv()
