dcdcf16b5b3148007d400e96ab4575003952bdd4
Brian <beavelar.github@gmail.com>
2021-10-26 21:16:12 -0600

    Wrap logic in try catch

--- scripts/historical.ps1
+++ scripts/historical.ps1
@@ -17,55 +17,60 @@ function Main {
 	$json = [ordered]@{}
 	$TextInfo = (Get-Culture).TextInfo
 
-	foreach ($line in $data) {
-		$alertDateString = $line.alert_time.Replace(" ", "T")
-		$highDateString = $line.high_date_time.Replace(" ", "T")
-		$lowDateString = $line.low_date_time.Replace(" ", "T")
-		$expireDate = Get-Date $line.expires_at
-		$alertDate = Get-Date $alertDateString
-		$highDate = Get-Date $highDateString
-		$lowDate = Get-Date $lowDateString
-		$highestAsk = 0
-		$pl = 0
-		$timePassed = 0
-		if (GetWinLoss $line.ask $line.low $lowDate $alertDate) {
-			$highestAsk = [double]$line.high
-			$pl = ([double]$line.high - [double]$line.ask)/[double]$line.ask
-			$timePassed = ($highDate - $alertDate).Days
+	try {
+		foreach ($line in $data) {
+			$alertDateString = $line.alert_time.Replace(" ", "T")
+			$highDateString = $line.high_date_time.Replace(" ", "T")
+			$lowDateString = $line.low_date_time.Replace(" ", "T")
+			$expireDate = Get-Date $line.expires_at
+			$alertDate = Get-Date $alertDateString
+			$highDate = Get-Date $highDateString
+			$lowDate = Get-Date $lowDateString
+			$highestAsk = 0
+			$pl = 0
+			$timePassed = 0
+			if (GetWinLoss $line.ask $line.low $lowDate $alertDate) {
+				$highestAsk = [double]$line.high
+				$pl = ([double]$line.high - [double]$line.ask)/[double]$line.ask
+				$timePassed = ($highDate - $alertDate).Days
+			}
+			else {
+				$highestAsk = [double]$line.low
+				$pl = ([double]$line.low/[double]$line.ask) - 1
+				$timePassed = ($lowDate - $alertDate).Days
+			}
+			$lineHashTable = @{
+				"ticker" = $line.ticker_symbol
+				"option_type" = $TextInfo.ToTitleCase($line.option_type)
+				"alert_time" = $alertDateString
+				"time_of_day" = $alertDateString.Split("T")[1].Replace("Z", "")
+				"expires" = $line.expires_at
+				"days_to_expiry" = ($expireDate - $alertDate).Days
+				"strike" = [double]$line.strike_price
+				"underlying" = [double]$line.underlying_purchase_price
+				"diff" = [double]$line.diff
+				"volume" = [int]$line.volume
+				"open_interest" = [int]$line.open_interest
+				"vol/oi" = [int]$line.volume/[int]$line.open_interest
+				"implied_volatility" = [double]$line.implied_volatility
+				"delta" = [double]$line.delta
+				"gamma" = [double]$line.gamma
+				"vega" = [double]$line.vega
+				"theta" = [double]$line.theta
+				"rho" = [double]$line.rho
+				"ask" = [int]$line.ask
+				"highest_ask" = $highestAsk
+				"p/l" = $pl
+				"time_passed" = $timePassed
+			}
+			$json.Add("$($line.ticker_symbol)|$($TextInfo.ToTitleCase($line.option_type))|$($alertDateString)", $lineHashTable)
 		}
-		else {
-			$highestAsk = [double]$line.low
-			$pl = ([double]$line.low/[double]$line.ask) - 1
-			$timePassed = ($lowDate - $alertDate).Days
-		}
-		$lineHashTable = @{
-			"ticker" = $line.ticker_symbol
-			"option_type" = $TextInfo.ToTitleCase($line.option_type)
-			"alert_time" = $alertDateString
-			"time_of_day" = $alertDateString.Split("T")[1].Replace("Z", "")
-			"expires" = $line.expires_at
-			"days_to_expiry" = ($expireDate - $alertDate).Days
-			"strike" = [double]$line.strike_price
-			"underlying" = [double]$line.underlying_purchase_price
-			"diff" = [double]$line.diff
-			"volume" = [int]$line.volume
-			"open_interest" = [int]$line.open_interest
-			"vol/oi" = [int]$line.volume/[int]$line.open_interest
-			"implied_volatility" = [double]$line.implied_volatility
-			"delta" = [double]$line.delta
-			"gamma" = [double]$line.gamma
-			"vega" = [double]$line.vega
-			"theta" = [double]$line.theta
-			"rho" = [double]$line.rho
-			"ask" = [int]$line.ask
-			"highest_ask" = $highestAsk
-			"p/l" = $pl
-			"time_passed" = $timePassed
-		}
-		$json.Add("$($line.ticker_symbol)|$($TextInfo.ToTitleCase($line.option_type))|$($alertDateString)", $lineHashTable)
+		$json | ConvertTo-Json | Out-File $outputJsonPath
+		Invoke-RestMethod -Uri $uri -Method POST -Body $json -ContentType "application/json"
 	}
-	$json | ConvertTo-Json | Out-File $outputJsonPath
-	Invoke-RestMethod -Uri $uri -Method POST -Body $json -ContentType "application/json"
+	catch {
+		Write-Host "Whoopsies"
+	}
 }
 
 Main $CSV_FILE_PATH $OUTPUT_JSON_PATH $HISTORICAL_ALERTS_URI
