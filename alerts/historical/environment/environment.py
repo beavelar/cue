578566bf45c9979e4401d7bb6dd50cf29d2e62a4
Brian <beavelar.github@gmail.com>
2021-11-01 22:54:34 -0600

    Environment and read json

--- alerts/historical/environment/environment.py
+++ alerts/historical/environment/environment.py
@@ -16,8 +16,6 @@ class environment:
 
 	This class will contain the environment variables for the historical alerts service:
 
-	- historical_alerts_dir
-
 	- historical_server_hostname
 
 	- historical_server_port
@@ -36,12 +34,10 @@ class environment:
 		logger.info('Retrieving environment variables')
 		try:
 			load_dotenv()
-			self.historical_alerts_dir = os.getenv('HISTORICAL_ALERTS_DIRECTORY', '')
 			self.historical_server_hostname = os.getenv('HISTORICAL_SERVER_HOSTNAME', '')
 			self.historical_server_port = os.getenv('HISTORICAL_SERVER_PORT', '')
 		except Exception as ex:
 			logger.critical('Failed to retrieve environment variables. Please verify environment variable exists')
 			logger.critical(str(ex))
-			self.historical_alerts_dir = ''
 			self.historical_server_hostname = ''
 			self.historical_server_port = ''
--- alerts/realtime/server.py
+++ alerts/realtime/server.py
@@ -1,10 +1,23 @@
-from flask import Flask
+import logging
+from flask import Flask, request
+from environment.environment import environment
+
+#########################################################################################################
+
+logging.basicConfig(format='%(levelname)s: %(asctime)s - %(name)s.%(funcName)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 app = Flask(__name__)
 
-# @app.route('/', methods = ['POST'])
-@app.route('/')
-def hello_world():
+#########################################################################################################
+
+@app.route('/', methods = ['POST'])
+def ingest():
+	data = request.json
 	return 'Hello World!'
 
+#########################################################################################################
+
 if __name__ == '__main__':
-	app.run()
+	env = environment()
+	app.run(host=env.realtime_server_hostname, port=env.realtime_server_port)
