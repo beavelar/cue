8010012a5552734bde050735d5ad5de3c0d582df
Brian <beavelar.github@gmail.com>
2021-11-04 21:13:51 -0600

    Proxy requests

--- proxy/src/server.ts
+++ proxy/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import httpProxy from 'http-proxy';
 import bodyParser from 'body-parser';
 import { Environment } from './env/env';
 import { Logger } from './logging/logger';
@@ -7,19 +8,38 @@ const logger = new Logger('server');
 const env = new Environment();
 if (env.validKeys()) {
   const server = express();
+  const proxy = new httpProxy();
   server.use(bodyParser.urlencoded({ extended: true }));
   server.use(bodyParser.json());
 
   server.post('/historical', (req, res) => {
-    logger.log('main', 'Receive POST request - /historical');
-    logger.log('main', JSON.stringify(req.body));
-    res.status(200).json('Okay :)');
+    logger.log('main', `Receive POST request - ${req.url}`);
+    const url = req.url.replace('/historical', '');
+    proxy.web(req, res, {
+      target: `http://${env.HISTORICAL_SERVER_HOSTNAME}:${env.HISTORICAL_SERVER_PORT}/${url}`
+    }, (err, req) => {
+      if (err) {
+        res.status(500).json('Oh no :(');
+      }
+      else {
+        res.status(200).json('Okay :)');
+      }
+    });
   });
 
   server.post('/realtime', (req, res) => {
-    logger.log('main', 'Receive POST request - /realtime');
-    logger.log('main', JSON.stringify(req.body));
-    res.status(200).json('Okay :)');
+    logger.log('main', `Receive POST request - ${req.url}`);
+    const url = req.url.replace('/realtime', '');
+    proxy.web(req, res, {
+      target: `http://${env.REALTIME_SERVER_HOSTNAME}:${env.REALTIME_SERVER_PORT}/${url}`
+    }, (err, req) => {
+      if (err) {
+        res.status(500).json('Oh no :(');
+      }
+      else {
+        res.status(200).json('Okay :)');
+      }
+    });
   });
 
   server.listen(env.PROXY_PORT, () => {
