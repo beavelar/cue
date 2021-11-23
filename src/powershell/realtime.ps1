e66e44422b62c320d868305e7b5e9a442a8fc61f
Brian <beavelar.github@gmail.com>
2021-11-23 14:51:07 -0700

    Realtime script and data directory rework

--- src/powershell/realtime.ps1
+++ src/powershell/realtime.ps1
@@ -1,34 +1,41 @@
-$FILE_NAME = "2021-10-01"
 $REALTIME_ALERTS_URI = "http://localhost:3001/realtime"
-$TXT_FILE_PATH = "$(Get-Location)\data\realtime-alerts\txt\$($FILE_NAME).txt"
-$OUTPUT_JSON_PATH = "$(Get-Location)\data\realtime-alerts\json\$($FILE_NAME).json"
 
-function TxtToJSON {
-	param($txt)
+# Variables to utilize if ingesting a single file
+# $FILE_NAME = "2021-10-01"
+# $INPUT_PATH = "$(Get-Location)\data\realtime-alerts\input\$($FILE_NAME).txt"
+# $OUTPUT_PATH = "$(Get-Location)\data\realtime-alerts\output\$($FILE_NAME).json"
+
+# Variables to utilize if ingesting a directory with files
+$INPUT_PATH = "$(Get-Location)\data\realtime-alerts\input"
+$OUTPUT_PATH = "$(Get-Location)\data\realtime-alerts\output"
+
+function InputToJSON {
+	param($inputData)
 	$json = [ordered]@{}
 	try {
-		for ($index = 0; $index -lt $txt.Length; $index = $index + 46) {
-			$header = $txt[$index].Split(" ")
+		Write-Host "Parsing ingested realtime data"
+		for ($index = 0; $index -lt $inputData.Length; $index = $index + 46) {
+			$header = $inputData[$index].Split(" ")
 			$ticker = $header[0].Replace("$", "")
 			$optionType = if ($header[2] -eq "C") { "Call" } else { "Put" }
-			$alertDate = Get-Date $txt[$index + 22]
+			$alertDate = Get-Date $inputData[$index + 22]
 			$alertDateString = $alertDate.ToString("yyyy-MM-ddThh:mm:ss")
 			$alertTimeString = $alertDate.ToString("hh:mm:ss")
 			$expiryDate = Get-Date $header[1]
 			$daysToExp = ($expiryDate - $alertDate).Days
-			$underlying = [double]$txt[$index + 16].Replace("$", "").Replace(",", "")
+			$underlying = [double]$inputData[$index + 16].Replace("$", "").Replace(",", "")
 			$strike = [double]$header[3].Replace("$", "").Replace(",", "")
 			$diff = if ($underlying -gt $strike) { (($strike / $underlying) - 1) * 100 } else { ($strike - $underlying) * 100 }
-			$volume = [int]$txt[$index + 28]
-			$openInterest = [int]$txt[$index + 26]
+			$volume = [int]$inputData[$index + 28]
+			$openInterest = [int]$inputData[$index + 26]
 			$vol_oi = $volume / $openInterest
-			$impliedVolatility = [double]$txt[$index + 30].Replace("%", "")
-			$delta = [double]$txt[$index + 32]
-			$gamma = [double]$txt[$index + 40]
-			$vega = [double]$txt[$index + 38]
-			$theta = [double]$txt[$index + 42]
-			$rho = [double]$txt[$index + 44]
-			$ask = [double]$txt[$index + 20].Replace("$", "")
+			$impliedVolatility = [double]$inputData[$index + 30].Replace("%", "")
+			$delta = [double]$inputData[$index + 32]
+			$gamma = [double]$inputData[$index + 40]
+			$vega = [double]$inputData[$index + 38]
+			$theta = [double]$inputData[$index + 42]
+			$rho = [double]$inputData[$index + 44]
+			$ask = [double]$inputData[$index + 20].Replace("$", "")
 	
 			$lineHashTable = @{
 				"ticker"             = $ticker
@@ -56,30 +63,49 @@ function TxtToJSON {
 		return ConvertTo-Json $json -Compress
 	}
 	catch {
-		Write-Host "Whoopsies"
+		Write-Host "An error occurred parsing incoming realtime data"
 	}
 	return $null
 }
-function Main {
-	param ($txtFilePath, $outputJsonPath, $uri)
-	if (-not (Test-Path -Path $txtFilePath -PathType Leaf)) {
-		Write-Host "No txt file :("
-		return
-	}
-	if ((Test-Path -Path $outputJsonPath -PathType Leaf) -and ($null -ne ($json = Get-Content $outputJsonPath))) {
+
+function IngestAlerts {
+	param ($inputFilePath, $outputFilePath, $uri)
+	if ((Test-Path -Path $outputFilePath -PathType Leaf) -and ($null -ne ($json = Get-Content $outputFilePath))) {
+		Write-Host "Executing POST request to: $($uri)"
 		Invoke-RestMethod -Uri $uri -Method POST -Body $json -ContentType "application/json"
 	}
 	else {
-		$data = Get-Content -Path $txtFilePath
-		$json = TxtToJSON $data
+		$data = Get-Content -Path $inputFilePath
+		$json = InputToJSON $data
 		if ($null -ne $json) {
-			$json | Out-File $outputJsonPath
+			Write-Host "Saving parsed realtime data to: $($outputFilePath)"
+			$json | Out-File $outputFilePath
+			Write-Host "Executing POST request to: $($uri)"
 			Invoke-RestMethod -Uri $uri -Method POST -Body $json -ContentType "application/json"
 		}
 		else {
-			Write-Host "Empty json :("
+			Write-Host "No realtime JSON data in the provided output path :("
 		}
 	}
 }
 
-Main $TXT_FILE_PATH $OUTPUT_JSON_PATH $REALTIME_ALERTS_URI
+function Main {
+	param ($inputPath, $outputPath, $uri)
+	if (Test-Path -Path $inputPath -PathType Leaf) {
+		IngestAlerts $inputPath $outputPath $uri
+	}
+	elseif (Test-Path -Path $inputPath -PathType Container) {
+		$files = Get-ChildItem -Path $inputPath
+		foreach ($file in $files) {
+			$inputFilePath = $file.FullName
+			$outputFilePath = "$($outputPath)\$($file.Basename).json"
+			IngestAlerts $inputFilePath $outputFilePath $uri
+		}
+	}
+	else {
+		Write-Host "Invalid realtime alerts path :("
+		return
+	}
+}
+
+Main $INPUT_PATH $OUTPUT_PATH $REALTIME_ALERTS_URI
