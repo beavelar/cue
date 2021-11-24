2d0dd2beeaa8decc21e05b2f674249b5c83c10f5
Brian <beavelar.github@gmail.com>
2021-11-23 21:12:13 -0700

    Create routes for proxy and db-store

--- src/node/database/src/server.ts
+++ src/node/database/src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { Environment } from './env/env';
 import { Logger } from './logging/logger';
+import { Historical } from './routes/historical';
+import { Realtime } from './routes/realtime';
 import { DBStore } from './store/db-store';
 
 const logger = new Logger('server');
@@ -14,89 +16,10 @@ if (env.validKeys()) {
     server.use(express.json({ limit: '50mb' }));
     server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
-    /**
-     * DB-Store realtime GET request
-     */
-    server.get('/realtime', (req, res) => {
-      logger.info('main', `Receive GET request - ${req.url}`);
-      store.getAllRealtime().then((data) => {
-        logger.info('main', 'Successfully retrieved realtime data from the database');
-        res.status(200).json(data);
-      }).catch((err) => {
-        logger.critical('main', 'An error occurred retrieving realtime data from the database', err);
-        res.status(500).json(err);
-      });
-    });
-
-    /**
-     * DB-Store realtime POST request
-     */
-    server.post('/historical', (req, res) => {
-      logger.info('main', `Receive POST request - ${req.url}`);
-      store.writeHistorical(req.body).then((result) => {
-        logger.info('main', 'Successfully wrote historical data to the database');
-        res.status(200).json(result);
-      }).catch((err) => {
-        logger.critical('main', 'An error occurred writing historical data to the database', err);
-        res.status(500).json(err);
-      });
-    });
-
-    /**
-     * DB-Store realtime DELETE request
-     */
-    server.delete('/historical', (req, res) => {
-      logger.info('main', `Receive DELETE request - ${req.url}`);
-      store.deleteAllHistorical().then((result) => {
-        logger.info('main', 'Successfully deleted historical data from the database');
-        res.status(200).json(result);
-      }).catch((err) => {
-        logger.critical('main', 'An error occurred deleting historical data from the database', err);
-        res.status(500).json(err);
-      });
-    });
-
-    /**
-     * DB-Store historical GET request
-     */
-    server.get('/historical', (req, res) => {
-      logger.info('main', `Receive GET request - ${req.url}`);
-      store.getAllHistorical().then((data) => {
-        logger.info('main', 'Successfully retrieved historical data from the database');
-        res.status(200).json(data);
-      }).catch((err) => {
-        logger.critical('main', 'An error occurred retrieving historical data from the database', err);
-        res.status(500).json(err);
-      });
-    });
-
-    /**
-     * DB-Store realtime POST request
-     */
-    server.post('/realtime', (req, res) => {
-      logger.info('main', `Receive POST request - ${req.url}`);
-      store.writeRealtime(req.body).then((result) => {
-        logger.info('main', 'Successfully rated and wrote realtime data to the database');
-        res.status(200).json(result);
-      }).catch((err) => {
-        logger.critical('main', 'An error occurred rating and/or writing realtime data to the database', err);
-        res.status(500).json(err);
-      });
-    });
-
-    /**
-     * DB-Store realtime DELETE request
-     */
-    server.delete('/realtime', (req, res) => {
-      logger.info('main', `Receive DELETE request - ${req.url}`);
-      store.deleteAllRealtime().then((result) => {
-        logger.info('main', 'Successfully deleted realtime data from the database');
-        res.status(200).json(result);
-      }).catch((err) => {
-        logger.critical('main', 'An error occurred deleting realtime data from the database', err);
-        res.status(500).json(err);
-      });
-    });
+    const realtime = new Realtime(new Logger('realtime'), store);
+    const historical = new Historical(new Logger('historical'), store);
+    server.use('/realtime', realtime.router);
+    server.use('/historical', historical.router);
 
     server.listen(env.DB_STORE_PORT, () => {
       logger.info('main', `Server is up and listening on port: ${env.DB_STORE_PORT}`);
--- src/node/proxy/src/server.ts
+++ src/node/proxy/src/server.ts
@@ -1,7 +1,8 @@
-import needle from 'needle';
 import express from 'express';
 import { Environment } from './env/env';
 import { Logger } from './logging/logger';
+import { Realtime } from './routes/realtime';
+import { Historical } from './routes/historical';
 
 const logger = new Logger('server');
 const env = new Environment();
@@ -10,174 +11,12 @@ if (env.validKeys()) {
   server.use(express.json({ limit: '50mb' }));
   server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
-  /**
-   * Historical GET proxy
-   */
-  server.get('/historical', (req, res) => {
-    logger.info('main', `Receive GET request - ${req.url}`);
-    req.url = req.url.replace('/historical', '');
-    const url = `http://${env.HISTORICAL_SERVER_HOSTNAME}:${env.HISTORICAL_SERVER_PORT}/`;
-
-    try {
-      needle.get(url, (err, _res) => {
-        if (err) {
-          const message = 'An error occurred proxying the GET request to the historical server';
-          logger.critical('main', message, err);
-          res.status(500).json(message);
-        }
-        else {
-          logger.info('main', 'Successfully proxied GET request to the historical server');
-          res.status(200).json(_res.body);
-        }
-      });
-    }
-    catch (ex) {
-      const message = 'An error occurred proxying the GET request to the historical server';
-      logger.critical('main', message, ex);
-      res.status(500).json(message);
-    }
-  });
-
-  /**
-   * Historical POST proxy
-   */
-  server.post('/historical', (req, res) => {
-    logger.info('main', `Receive POST request - ${req.url}`);
-    req.url = req.url.replace('/historical', '');
-    const url = `http://${env.HISTORICAL_SERVER_HOSTNAME}:${env.HISTORICAL_SERVER_PORT}/`;
-
-    try {
-      needle.post(url, req.body, { json: true }, (err, _res) => {
-        if (err) {
-          const message = 'An error occurred proxying the POST request to the historical server';
-          logger.critical('main', message, err);
-          res.status(500).json(message);
-        }
-        else {
-          logger.info('main', 'Successfully proxied POST request to the historical server');
-          res.status(200).json(_res.body);
-        }
-      });
-    }
-    catch (ex) {
-      const message = 'An error occurred proxying the POST request to the historical server';
-      logger.critical('main', message, ex);
-      res.status(500).json(message);
-    }
-  });
-
-  /**
-   * Historical DELETE proxy
-   */
-  server.delete('/historical', (req, res) => {
-    logger.info('main', `Receive DELETE request - ${req.url}`);
-    req.url = req.url.replace('/historical', '');
-    const url = `http://${env.HISTORICAL_SERVER_HOSTNAME}:${env.HISTORICAL_SERVER_PORT}/`;
-
-    try {
-      needle.delete(url, {}, (err, _res) => {
-        if (err) {
-          const message = 'An error occurred proxying the DELETE request to the historical server';
-          logger.critical('main', message, err);
-          res.status(500).json(message);
-        }
-        else {
-          logger.info('main', 'Successfully proxied DELETE request to the historical server');
-          res.status(200).json(_res.body);
-        }
-      });
-    }
-    catch (ex) {
-      const message = 'An error occurred proxying the DELETE request to the historical server';
-      logger.critical('main', message, ex);
-      res.status(500).json(message);
-    }
-  });
-
-  /**
-   * Realtime GET proxy
-   */
-  server.get('/realtime', (req, res) => {
-    logger.info('main', `Receive GET request - ${req.url}`);
-    req.url = req.url.replace('/realtime', '');
-    const url = `http://${env.REALTIME_SERVER_HOSTNAME}:${env.REALTIME_SERVER_PORT}/`;
-
-    try {
-      needle.get(url, (err, _res) => {
-        if (err) {
-          const message = 'An error occurred proxying the GET request to the realtime server';
-          logger.critical('main', message, err);
-          res.status(500).json(message);
-        }
-        else {
-          logger.info('main', 'Successfully proxied GET request to the realtime server');
-          res.status(200).json(_res.body);
-        }
-      });
-    }
-    catch (ex) {
-      const message = 'An error occurred proxying the GET request to the realtime server';
-      logger.critical('main', message, ex);
-      res.status(500).json(message);
-    }
-  });
-
-  /**
-   * Realtime POST proxy
-   */
-  server.post('/realtime', (req, res) => {
-    logger.info('main', `Receive POST request - ${req.url}`);
-    const url = `http://${env.REALTIME_SERVER_HOSTNAME}:${env.REALTIME_SERVER_PORT}/`;
-
-    try {
-      needle.post(url, req.body, { json: true }, (err, _res) => {
-        if (err) {
-          const message = 'An error occurred proxying the POST request to the realtime server';
-          logger.critical('main', message, err);
-          res.status(500).json(message);
-        }
-        else {
-          logger.info('main', 'Successfully proxied POST request to the realtime server');
-          res.status(200).json(_res.body);
-        }
-      });
-    }
-    catch (ex) {
-      const message = 'An error occurred proxying the POST request to the realtime server';
-      logger.critical('main', message, ex);
-      res.status(500).json(message);
-    }
-  });
+  const realtime = new Realtime(env, new Logger('realtime'));
+  const historical = new Historical(env, new Logger('historical'));
+  server.use('/realtime', realtime.router);
+  server.use('/historical', historical.router);
 
   server.listen(env.PROXY_PORT, () => {
     logger.info('main', `Server is up and listening on port: ${env.PROXY_PORT}`);
-  });
-
-  /**
-   * Realtime DELETE proxy
-   */
-  server.delete('/realtime', (req, res) => {
-    logger.info('main', `Receive DELETE request - ${req.url}`);
-    req.url = req.url.replace('/realtime', '');
-    const url = `http://${env.REALTIME_SERVER_HOSTNAME}:${env.REALTIME_SERVER_PORT}/`;
-
-    try {
-      needle.delete(url, {}, (err, _res) => {
-        if (err) {
-          const message = 'An error occurred proxying the DELETE request to the realtime server';
-          logger.critical('main', message, err);
-          res.status(500).json(message);
-        }
-        else {
-          logger.info('main', 'Successfully proxied DELETE request to the realtime server');
-          res.status(200).json(_res.body);
-        }
-      });
-    }
-    catch (ex) {
-      const message = 'An error occurred proxying the DELETE request to the realtime server';
-      logger.critical('main', message, ex);
-      res.status(500).json(message);
-    }
   });
 }
