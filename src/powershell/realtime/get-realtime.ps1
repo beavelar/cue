ce9ddb68fa63ddb00dec45560e230d8340dfa069
Brian <beavelar.github@gmail.com>
2021-12-14 20:59:33 -0700

    Score realtime alerts when parsing them to csv

--- src/powershell/realtime/get-realtime.ps1
+++ src/powershell/realtime/get-realtime.ps1
@@ -13,7 +13,7 @@ function JSONToCSV {
 	param ($json)
 	$data = ConvertFrom-Json $json
 	$sb = [System.Text.StringBuilder]::new()
-	[void]$sb.Append("Ticker,Option Type,Alerted At,Day of Week,Time of Day,Expiry,Days to Exp.,Strike,Underlying,Diff %,Volume,Open Interest,Vol/OI,Implied Volatility,Delta,Gamma,Vega,Theta,Rho,Alert Ask`n")
+	[void]$sb.Append("Ticker,Option Type,Alerted At,Day of Week,Time of Day,Expiry,Days to Exp.,Strike,Underlying,Diff %,Volume,Open Interest,Vol/OI,Implied Volatility,Delta,Gamma,Vega,Theta,Rho,Alert Ask,Score`n")
 	$mtTimezone = Get-TimeZone -Id "Mountain Standard Time" 
 	foreach ($line in $data) {
 		$alertDate = Get-Date -UnixTimeSeconds $line.alert_date -Format "yyyy-MM-dd HH:mm"
@@ -25,9 +25,40 @@ function JSONToCSV {
 		$vol_oi = [math]::Round(($line."vol/oi".value * 100), 2)
 		$impliedVol = [math]::Round(($line.implied_volatility.value * 100), 2)
 		$ask = $line.ask.value * 100
-		[void]$sb.Append("$($line.ticker),$($line.option_type),$($alertDate),$($line.day_of_week.rate)|$($dayOfWeek),$($line.time_of_day.rate)|$($timeOfDay),$($expiry),$($line.days_to_expiry.rate)|$($line.days_to_expiry.value),$($line.strike.rate)|`$$($line.strike.value),$($line.underlying.rate)|`$$($line.underlying.value),$($line.diff.rate)|$($diff)%,$($line.volume.rate)|$($line.volume.value),$($line.open_interest.rate)|$($line.open_interest.value),$($line."vol/oi".rate)|$($vol_oi),$($line.implied_volatility.rate)|$($impliedVol)%,$($line.delta.rate)|$($line.delta.value),$($line.gamma.rate)|$($line.gamma.value),$($line.vega.rate)|$($line.vega.value),$($line.theta.rate)|$($line.theta.value),$($line.rho.rate)|$($line.rho.value),$($line.ask.rate)|`$$($ask)`n")
+		$score = [math]::Round(((CalculateScore $line) * 100), 2)
+		[void]$sb.Append("$($line.ticker),$($line.option_type),$($alertDate),$($line.day_of_week.rate)|$($dayOfWeek),$($line.time_of_day.rate)|$($timeOfDay),$($expiry),$($line.days_to_expiry.rate)|$($line.days_to_expiry.value),$($line.strike.rate)|`$$($line.strike.value),$($line.underlying.rate)|`$$($line.underlying.value),$($line.diff.rate)|$($diff)%,$($line.volume.rate)|$($line.volume.value),$($line.open_interest.rate)|$($line.open_interest.value),$($line."vol/oi".rate)|$($vol_oi),$($line.implied_volatility.rate)|$($impliedVol)%,$($line.delta.rate)|$($line.delta.value),$($line.gamma.rate)|$($line.gamma.value),$($line.vega.rate)|$($line.vega.value),$($line.theta.rate)|$($line.theta.value),$($line.rho.rate)|$($line.rho.value),$($line.ask.rate)|`$$($ask),$($score)%`n")
 	}
 	return $sb.ToString()
+}
+
+function CalculateScore {
+	param($alert)
+	return ((RateToNumber $($line.day_of_week.rate)) +
+	(RateToNumber $($line.time_of_day.rate)) +
+	(RateToNumber $($line.days_to_expiry.rate)) +
+	(RateToNumber $($line.strike.rate)) +
+	(RateToNumber $($line.underlying.rate)) +
+	(RateToNumber $($line.diff.rate)) +
+	(RateToNumber $($line.volume.rate)) +
+	(RateToNumber $($line.open_interest.rate)) +
+	(RateToNumber $($line."vol/oi".rate)) +
+	(RateToNumber $($line.implied_volatility.rate)) +
+	(RateToNumber $($line.delta.rate)) +
+	(RateToNumber $($line.gamma.rate)) +
+	(RateToNumber $($line.vega.rate)) +
+	(RateToNumber $($line.theta.rate)) +
+	(RateToNumber $($line.rho.rate)) +
+	(RateToNumber $($line.ask.rate))) / 64
+}
+
+function RateToNumber {
+	param ($value)
+	switch ($value) {
+		"BAD" { return 1; Break }
+		"OKAY" { return 2; Break }
+		"GOOD" { return 3; Break }
+		"BEST" { return 4; Break }
+	}
 }
 
 function NumberToDayOfWeek {
